b795aa4b5d3d523353b974bde852bd913d7e4080
Ryo <134986643+Ryo511@users.noreply.github.com>
2024-05-21 14:08:49 +0900

    Initial Commit

---
+++ IOS203PhotoMap_33/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS203PhotoMap_33
+//
+//  Created by OLIVER LIAO on 2024/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS203PhotoMap_33/IOS203PhotoMap_33App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS203PhotoMap_33App.swift
+//  IOS203PhotoMap_33
+//
+//  Created by OLIVER LIAO on 2024/5/21.
+//
+
+import SwiftUI
+
+@main
+struct IOS203PhotoMap_33App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
